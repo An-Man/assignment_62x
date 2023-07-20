b48c06c8c5362f2edc9617b1b39430c2c3cf4563
An-Man <anna.pikkarainen@gmail.com>
2023-07-20 10:09:57 +0300

    a641 ex 1 finished

---
+++ a641/ex_1/main.cpp
@@ -0,0 +1,17 @@
+// Exercise 1
+
+// Create a namespace and a type in the global namespace.  Create 
+// a function in the namespace and a static member function 
+// for the type.  Try using them from the global namespace.
+
+#include "example.h"
+#include <iostream>
+
+int main()
+{
+    std::cout << Lapland::return_town() << '\n';
+
+    std::cout << Reindeer::return_10() << '\n';
+
+    return 0;
+}
